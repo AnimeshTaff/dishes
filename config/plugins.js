1838363587fc40738cf47acb52dad1d1985c97ca
Animesh <animesh.kumar@taffinc.com>
2025-04-12 17:57:26 +0530

    add custom plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,8 @@
-module.exports = () => ({});
+module.exports = {
+    // ...
+    'receipe': {
+      enabled: true,
+      resolve: './src/plugins/receipe'
+    },
+    // ...
+  }
---
+++ src/plugins/receipe/README.md
@@ -0,0 +1,3 @@
+# receipe
+
+this is all receipe
